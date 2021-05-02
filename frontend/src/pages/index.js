6ddd84801034c6c937715f096f6f4c9c81c07f99
sally0226 <sally734595@gmail.com>
2021-05-02 15:53:37 +0900

    fix error

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as Main } from './Main';
-export { default as LogIn } from './LogIn';
+export { default as LogIn } from './Login';
 export { default as colorTest } from './colorTest';
 export { default as MyPage } from './MyPage';
 export { default as Register } from './Register';
--- frontend/src/pages/logIn.js
+++ frontend/src/pages/logIn.js
@@ -6,8 +6,8 @@ function LogIn() {
 		<div className="login">
 			<LogInForm />
 			<div className="appendix">
-				<a className="text" href="/FindPw">비밀번호가 기억이 안나십니까?</a>
-				<a className="text" href="/Join">저희 사이트가 처음이시라구요?</a>
+				<a className="text" href="/findpw">비밀번호가 기억이 안나십니까?</a>
+				<a className="text" href="/register">저희 사이트가 처음이시라구요?</a>
 			</div>
 		</div>
 
