5bf8099e6b6160a2c7e6cdf41df4e4f6732fbf00
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-05-05 23:09:55 +0900

    챌린지 목록 페이지 시작
    그리드 체크 및 css 체크

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, Grid, Button, Tabs, Tab } from '@material-ui/core';
 
 function Main() {
+	const [ing, setIng] = useState("");
+	const [fin, setFin] = useState("test@gmail.com");
+
 	return (
 		<>
-			<div className="cssTest">
-				<p className="pTest">LandingPage</p>
+			<div className="landing">
+				<Grid container spacing={3} className="cha_list">
+
+					<Grid item xs={12}>
+						참여중인 챌린지
+					</Grid>
+					<Grid item xs={3}>
+						<Box className="cha_box">
+							1일 1커밋
+						</Box>
+					</Grid>
+					<Grid item xs={3}>
+						<Box className="cha_box">
+							2일 1커밋
+						</Box>
+					</Grid>
+					<Grid item xs={3}>
+						<Box className="cha_box">
+							1일 1운동
+						</Box>
+					</Grid>
+				</Grid>
 			</div>
 		</>
 	);
