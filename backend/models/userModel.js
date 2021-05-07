c5ea63a99d75b8f90bd7d665da912e61176a7ebf
sally0226 <sally734595@gmail.com>
2021-05-07 17:28:58 +0900

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,30 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// var User = new Schema({
-//   User_id:String,
-//   User_name: String,
-//   User_password: String,
-//   email: String,
-//   git_account:String,
-//   challenge_count:String,
-//   in_date:Date,
-// });
-
 var User = new Schema({
 	user_id:{type: String, required: true, unique: true, trim: true, lowercase: true,},
 	user_pw: {type: String, required: true, trim: true},
 	user_name: {type: String, required: true, trim: true},
 	user_email: {type: String, required: true, trim: true},
 	git_id:{type: String, required: true, trim: true},
-	ch_ing:{type: Array, required: false},
-	ch_end:{type: Array, required: false},
+	ch_ing:[String], //challenge 스키마 배열로 변경해도 좋을듯 
+	ch_end:[String],
 	in_date:{type: Date, required: false},
 	last_update:{type: Date, required: true}
 });
 
 User.statics.create = function(user_id,user_pw,user_name,user_email,git_id,ch_ing,ch_end,in_date,last_update) {
-	const challenge = new this({
+	const user = new this({
 		user_id,
 		user_pw,
 		email,
@@ -40,7 +30,7 @@ User.statics.create = function(user_id,user_pw,user_name,user_email,git_id,ch_in
   console.log('user만들어짐 :'+user_id);
   
   // return the Promise
-  return challenge.save()
+  return user.save()
 }
 
 
