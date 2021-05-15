2a01ae3b5da9ebcb756b8cf538c85b63c63782d8
Hyun-git <adius2845@naver.com>
2021-05-15 14:52:15 +0900

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,11 @@ db.once('open', function(){
     console.log('Connection success!');
 });
 
+<<<<<<< Updated upstream
 app.use(express.json());
 
+=======
+>>>>>>> Stashed changes
 app.use('/',router);
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
