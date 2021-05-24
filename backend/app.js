b85cef3243627b8c2220fd3df5b217f79aceeb20
Hyun-git <adius2845@naver.com>
2021-05-24 20:26:28 +0900

    up

--- backend/app.js
+++ backend/app.js
@@ -15,17 +15,6 @@ mongoose.connect(config.mongoURI,{
 .catch(err => console.log(err));
 
 
-// 연결이 안되면
-db.on('error',function(){
-    console.log('Connection error!');
-});
-
-// 연결이 되면
-db.once('open', function(){
-    console.log('Connection success!');
-
-});
-
 app.use('/',router);
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
