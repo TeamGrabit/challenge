617998212a664231743c4f4bb872b75243cb3430
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-05-30 15:03:35 +0900

    ManageChallenge 진행
    vertical Tabs css 수정

--- frontend/src/pages/ManageChallenge.js
+++ frontend/src/pages/ManageChallenge.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Grid, Tabs, Tab, Typography } from '@material-ui/core';
+import { Button, Grid, Tabs, Tab, makeStyles, withStyles, Typography } from '@material-ui/core';
 import { useChallengeState } from '../MVVM/Model/ChallengeModel';
 import ManageComponent from '../components/ManageComponent';
 
@@ -14,27 +14,63 @@ function ManageChallenge({ match }) {
 	}, [CId, challengeData]);
 
 	const [value, setValue] = useState(0);
-
 	const handleChange = (event, newValue) => {
 		setValue(newValue);
 	};
 
+	const useStyles = makeStyles((theme) => ({
+		root: {
+			flexGrow: 1,
+			backgroundColor: theme.palette.background.paper,
+			display: 'flex',
+			height: '100vh',
+		},
+		tabs: {
+			borderRight: `0px solid ${theme.palette.divider}`,
+		},
+	}));
+	const classes = useStyles();
+
+	const ManTab = withStyles((theme) => ({
+		root: {
+			textTransform: 'none',
+			fontSize: theme.typography.pxToRem(16),
+			marginRight: theme.spacing(1),
+			backgroundColor: '#CCFCCB',
+			fontFamily: [
+				'맑은 고딕',
+				'Arial',
+				'sans-serif',
+			].join(','),
+			'&$selected': {
+				fontWeight: theme.typography.fontWeightBold,
+				backgroundColor: '#96E6B3',
+			},
+			'&:hover': {
+				fontWeight: theme.typography.fontWeightBold,
+			},
+		},
+		selected: {},
+	}))((props) => <Tab disableRipple {...props} />);
+
 	return (
 		<div className="ManageChallenge">
-			<div className="info">
+			<Typography>{title}</Typography>
+			<div className={classes.root}>
 				<Tabs
 					orientation="vertical"
-					className="man_tab"
+					className={classes.tabs}
 					value={value}
 					onChange={handleChange}
 					aria-label="ant example"
+					indicatorColor="transparent"
 				>
-					<Tab label="Item One" />
-					<Tab label="Item Two" />
-					<Tab label="Item Three" />
-					<Tab label="Item Four" />
-					<Tab label="Item Five" />
-					<Tab label="Item Six" />
+					<ManTab label="챌린지 이름 관리" />
+					<ManTab label="챌린지 멤버 관리" />
+					<ManTab label="챌린지 날짜 관리" />
+					<ManTab label="..." />
+					<ManTab label="..." />
+					<ManTab label="챌린지 중단" />
 				</Tabs>
 				<ManageComponent value={value} index={0} />
 				<ManageComponent value={value} index={1} />
