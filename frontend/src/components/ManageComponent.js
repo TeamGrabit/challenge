604259e130fdf66a4d8e2c20c896221f9e51b7d1
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-06-23 23:34:47 +0900

    챌린지 관리 페이지 진행
    manageComponent 진행

--- frontend/src/components/ManageComponent.js
+++ frontend/src/components/ManageComponent.js
@@ -3,16 +3,27 @@ import { Button, Grid, Box, Typography } from '@material-ui/core';
 
 function ManageComponent({ value, index }) {
 	return (
-		<div>
-			<div className="manageComponent">
-				<div>
-					{value === index && (
-						<Box>
-							<Typography>{value}</Typography>
-						</Box>
-					)}
+		<div className="manageComponent">
+			{value === index && value === 0 && (
+				<div className="comp_title">
+					<div>챌린지 이름 관리</div>
 				</div>
-			</div>
+			)}
+			{value === index && value === 1 && (
+				<div>
+					<div>챌린지 멤버 관리</div>
+				</div>
+			)}
+			{value === index && value === 2 && (
+				<div>
+					<div>챌린지 날짜 관리</div>
+				</div>
+			)}
+			{value === index && value === 5 && (
+				<div>
+					<div>챌린지 중단</div>
+				</div>
+			)}
 		</div>
 	);
 }
