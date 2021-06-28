d5807f779f939ff49323891d7d7cb7b9d062712c
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-06-28 17:43:36 +0900

    챌린지 관리 페이지
    챌린지 멤버 관리 css

--- frontend/src/components/ManageComponent.js
+++ frontend/src/components/ManageComponent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, TextField } from '@material-ui/core';
+import { Grid, Button, TextField, Typography } from '@material-ui/core';
 import { useChallengeState } from '../MVVM/Model/ChallengeModel';
 
 function ManageComponent({ value, index, CId }) {
@@ -22,51 +22,67 @@ function ManageComponent({ value, index, CId }) {
 				<div className="comp_box">
 					<div className="title_box">
 						<div className="comp_title">챌린지 이름 관리</div>
-						<Button className="saveBtn">수정</Button>
+						<Button className="saveBtn">저장</Button>
 					</div>
-					<TextField
-						variant="outlined"
-						inputProps={{ style: { fontSize: 20 } }}
-						margin="dense"
-						className="txt_title"
-						value={title}
-					/>
+					<div className="content_box">
+						<TextField
+							variant="outlined"
+							inputProps={{ style: { fontSize: 20 } }}
+							margin="dense"
+							className="txt_title"
+							value={title}
+						/>
+					</div>
 				</div>
 			)}
 			{value === index && value === 1 && (
 				<div className="comp_box">
 					<div className="title_box">
 						<div className="comp_title">챌린지 멤버 관리</div>
-						<Button className="saveBtn">수정</Button>
+						<Button className="saveBtn">저장</Button>
 					</div>
-					<div>{user}</div>
+					<div className="content_box">
+						<Grid container spacing={1}>
+							{user.map((c) => (
+								<Grid item lg={3} md={4} sm={6} xs={12}>
+									<div className="member_box">
+										<div className="member_inner">
+											{c}
+										</div>
+									</div>
+								</Grid>
+							))}
+						</Grid>
+					</div>
 				</div>
 			)}
 			{value === index && value === 2 && (
 				<div className="comp_box">
 					<div className="title_box">
 						<div className="comp_title">챌린지 날짜 관리</div>
-						<Button className="saveBtn">수정</Button>
+						<Button className="saveBtn">저장</Button>
 					</div>
-					<div>
-						{sDate.getFullYear()}
-						년
-						{" "}
-						{sDate.getMonth()}
-						월
-						{" "}
-						{sDate.getDate()}
-						일
-					</div>
-					<div>
-						{eDate.getFullYear()}
-						년
-						{" "}
-						{eDate.getMonth()}
-						월
-						{" "}
-						{eDate.getDate()}
-						일
+					<div className="content_box">
+						<div>
+							{sDate.getFullYear()}
+							년
+							{" "}
+							{sDate.getMonth()}
+							월
+							{" "}
+							{sDate.getDate()}
+							일
+						</div>
+						<div>
+							{eDate.getFullYear()}
+							년
+							{" "}
+							{eDate.getMonth()}
+							월
+							{" "}
+							{eDate.getDate()}
+							일
+						</div>
 					</div>
 				</div>
 			)}
@@ -74,6 +90,9 @@ function ManageComponent({ value, index, CId }) {
 				<div className="comp_box">
 					<div className="title_box">
 						<div className="comp_title">챌린지 중단</div>
+					</div>
+					<div className="content_box">
+						<Typography>정말 중단하시겠습니까?</Typography>
 					</div>
 				</div>
 			)}
