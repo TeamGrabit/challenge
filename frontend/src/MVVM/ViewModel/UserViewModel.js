a3cb0880ce2071c6ad0f3827d204a58f38653582
sally0226 <sally0226@naver.com>
2021-06-30 13:28:45 +0900

    Login logout front (#61)
    * login/logout back과 연동
    
    - TODO : 로그인 시에 등록된 git_id로 프로필 사진 불러오도록 수정하기
    
    * git profile img 불러오기

--- frontend/src/MVVM/ViewModel/UserViewModel.js
+++ frontend/src/MVVM/ViewModel/UserViewModel.js
@@ -1,40 +1,64 @@
 import React, { useContext, createContext } from 'react';
+import axios from 'axios';
 import { useUserState, useUserDispatch } from '../Model/UserModel';
 
 const LoginUserContext = createContext((id, pw) => {});
 const LogoutUserContext = createContext(() => {});
+const VerifyUserContext = createContext(() => {});
 
 export const UserLogicProvider = ({ children }) => {
 	const user = useUserState();
 	const userDispatch = useUserDispatch();
 
-	const LoginUser = (id, pw) => {
-		if (id === 123 && pw === 123) {
-			// 임시 유저 id=123, pw=123
+	const VerifyUser = async () => {
+		await axios.post("http://localhost:5000/auth/jwtvalidcheck", {}, {
+			withCredentials: true,
+		}).then((res) => {
+			console.log(res.data);
 			userDispatch({
-				...user, // user에 다른 속성이 있을 경우 가져오려고
+				...res.data,
 				auth: "user"
 			});
-			console.log("로그인 성공");
-			return true;
-		}
-		return false;
+		});
+		console.log(user);
+	};
+	const LoginUser = async (id, pw) => {
+		let flag = false;
+		console.log("Dddddddddddd");
+		await axios.post('http://localhost:5000/login', { userId: id, userPw: pw }, {
+			withCredentials: true,
+		}).then((res) => {
+			console.log(res.data.result);
+			if (res.data.result === "ok") flag = true;
+		});
+		await VerifyUser();
+		return flag;
 	};
 
-	const LogoutUser = () => {
-		if (user.auth === "user") {
-			userDispatch({
-				...user,
-				auth: "no"
-			});
-			console.log("로그아웃 성공");
-		}
+	const LogoutUser = async () => {
+		// if (user.auth === "user") {
+		// 	userDispatch({
+		// 		...user,
+		// 		auth: "no"
+		// 	});
+		// 	console.log("로그아웃 성공");
+		// }
+		let flag = false;
+		await axios.post('http://localhost:5000/logout', {}, {
+			withCredentials: true,
+		}).then((res) => {
+			console.log(res.data.logoutSuccess);
+			if (res.data.logoutSuccess === true) flag = true;
+		});
+		return flag;
 	};
 
 	return (
 		<LoginUserContext.Provider value={LoginUser}>
 			<LogoutUserContext.Provider value={LogoutUser}>
-				{children}
+				<VerifyUserContext.Provider value={VerifyUser}>
+					{children}
+				</VerifyUserContext.Provider>
 			</LogoutUserContext.Provider>
 		</LoginUserContext.Provider>
 	);
@@ -47,5 +71,10 @@ export function useLoginUser() {
 
 export function useLogoutUser() {
 	const context = useContext(LogoutUserContext);
+	return context;
+}
+
+export function useVerifyUser() {
+	const context = useContext(VerifyUserContext);
 	return context;
 }
