b3dd853c621559ee95a0d3cbec9e31c425a5a8ba
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-07-01 17:49:18 +0900

    챌린지 생성 페이지 리팩토링
    <Grid>, <Box> 삭제
    날짜 input -> <DatePicker>로 변경

--- frontend/src/pages/ChallengeMake.js
+++ frontend/src/pages/ChallengeMake.js
@@ -1,16 +1,16 @@
-import React, { useState, useRef } from 'react';
-import { Link, Box, Grid, Button, TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Link, Button, TextField } from '@material-ui/core';
+import DatePicker from 'react-datepicker';
 
 function ChallengeMake() {
 	const [Name, setName] = useState("");
 	const [Category, setCategory] = useState("");
 	const [CateSel, setCateSel] = useState("notsel");
-	const [sttDate, setSttdate] = useState("");
-	const [finDate, setFindate] = useState("");
+	const [sDate, setsDate] = useState("");
+	const [eDate, seteDate] = useState("");
 	const [Intro, setIntro] = useState("");
-	const sttRef = useRef(null);
-	const finRef = useRef(null);
-	const selRef = useRef(null);
+	const [dateRef, setDateRef] = useState(true);
+	const [selRef, setSelRef] = useState(true);
 
 	const changeName = (e) => {
 		setName(e.currentTarget.value);
@@ -21,113 +21,86 @@ function ChallengeMake() {
 	const changeCateSel = (e) => {
 		setCateSel(e.currentTarget.value);
 		if (e.currentTarget.value === "") {
-			selRef.current.disabled = false;
+			setSelRef(false);
 		} else {
-			selRef.current.disabled = true;
+			setSelRef(true);
 		}
-	};
-	const changeSttdate = (e) => {
-		setSttdate(e.currentTarget.value);
-	};
-	const changeFindate = (e) => {
-		setFindate(e.currentTarget.value);
 	};
 	const changeIntro = (e) => {
 		setIntro(e.currentTarget.value);
 	};
-
 	const handleCheck = () => {
-		if (sttRef.current.disabled === true) {
-			sttRef.current.disabled = false;
+		if (dateRef === true) {
+			setDateRef(false);
 		} else {
-			sttRef.current.disabled = true;
-		}
-		if (finRef.current.disabled === true) {
-			finRef.current.disabled = false;
-		} else {
-			finRef.current.disabled = true;
-		}
-	};
-
-	const handleSelect = () => {
-		if (selRef.current.disabled === true) {
-			selRef.current.disabled = false;
-		} else {
-			selRef.current.disabled = true;
+			setDateRef(true);
 		}
 	};
 
 	return (
 		<div className="challengeMake">
 			<div className="makefield">
-				<Grid item xs={12} className="name_grid">
-					<Grid container alignItems="center">
-						<Grid>
-							<Box className="cha_name">
-								챌린지명
-							</Box>
-						</Grid>
-						<Grid>
-							<input value={Name} onChange={changeName} className="name_txtfield" />
-						</Grid>
-					</Grid>
-				</Grid>
-				<Grid item xs={12} className="name_grid">
-					<Grid container alignItems="center">
-						<Grid>
-							<Box className="cha_name">
-								카테고리
-							</Box>
-						</Grid>
-						<Grid>
-							<input disabled value={Category} onChange={changeCategory} ref={selRef} className="category_txtfield" size="10" />
-						</Grid>
-						<Grid>
-							<select id="selbox" value={CateSel} onChange={changeCateSel} className="category_selfield">
-								<option value="커밋">커밋</option>
-								<option value="운동">운동</option>
-								<option value="공부">공부</option>
-								<option value="기타">기타</option>
-								<option value="">직접입력</option>
-							</select>
-						</Grid>
-					</Grid>
-				</Grid>
-				<Grid item xs={12} className="name_grid">
-					<Grid container alignItems="center">
-						<Grid>
-							<input type="checkbox" onChange={handleCheck} className="date_check" />
-						</Grid>
-						<Grid>
-							<Box className="cha_name">
-								시작날짜
-							</Box>
-						</Grid>
-						<Grid>
-							<input disabled value={sttDate} onChange={changeSttdate} ref={sttRef} className="date_txtfield" size="11" />
-						</Grid>
-						<Grid>
-							<Box className="cha_name">
-								종료날짜
-							</Box>
-						</Grid>
-						<Grid>
-							<input disabled value={finDate} onChange={changeFindate} ref={finRef} className="date_txtfield" size="11" />
-						</Grid>
-					</Grid>
-				</Grid>
-				<Grid item xs={12}>
-					<Grid container>
-						<Grid>
-							<Box className="cha_name">
-								그룹소개
-							</Box>
-						</Grid>
-						<Grid>
-							<textarea value={Intro} onChange={changeIntro} className="intro_txtfield" />
-						</Grid>
-					</Grid>
-				</Grid>
+				<div className="info_grid">
+					<div className="info_title">
+						챌린지명
+					</div>
+					<TextField
+						className="name_txt"
+						variant="outlined"
+						inputProps={{ style: { fontSize: 20 } }}
+						margin="dense"
+						onChange={changeName}
+					/>
+				</div>
+				<div className="info_grid">
+					<div className="info_title">
+						카테고리
+					</div>
+					<TextField
+						className="category_txt"
+						variant="outlined"
+						inputProps={{ style: { fontSize: 20 } }}
+						disabled={selRef}
+						value={Category}
+						onChange={changeCategory}
+					/>
+					<select className="category_selfield" id="selbox" value={CateSel} onChange={changeCateSel}>
+						<option value="커밋">커밋</option>
+						<option value="운동">운동</option>
+						<option value="공부">공부</option>
+						<option value="기타">기타</option>
+						<option value="">직접입력</option>
+					</select>
+				</div>
+				<div className="info_grid">
+					<input className="date_check" type="checkbox" onChange={handleCheck} />
+					<div className="info_title">
+						시작날짜
+					</div>
+					<DatePicker
+						disabled={dateRef}
+						className="date_pick"
+						selected={sDate}
+						onChange={(date) => setsDate(date)}
+						dateFormat="yyyy. MM. dd"
+					/>
+					<div className="info_title">
+						종료날짜
+					</div>
+					<DatePicker
+						disabled={dateRef}
+						className="date_pick"
+						selected={eDate}
+						onChange={(date) => seteDate(date)}
+						dateFormat="yyyy. MM. dd"
+					/>
+				</div>
+				<div className="intro_grid">
+					<div className="info_title">
+						그룹소개
+					</div>
+					<textarea value={Intro} onChange={changeIntro} className="intro_txtfield" />
+				</div>
 				<div className="btn_out">
 					<Link className="link" href="/challenge">
 						<Button className="btn_cancel">
