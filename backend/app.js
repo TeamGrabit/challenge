cda2cd1428e2b55edc34dd6897071a6881c36b40
MOBUMIN <kiju23@naver.com>
2021-08-05 15:23:48 +0900

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -42,14 +42,14 @@ app.use(expressSession({
 }))
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.get('/test', (req,res) => res.send('hi'));
+app.use('/',router);
 if(process.env.NODE_ENV === "production"){
-	app.use(express.static("../frontend/build"));
+	// app.use(express.static("../frontend/build"));
 	app.get("*", (req,res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+		res.sendFile(path.join(__dirname+"../frontend/build/index.html"));
 	})
 }
 else app.get('/', (req,res)=>{res.send('hi')})
-app.get('/test', (req,res) => res.send('hi'));
-app.use('/',router);
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
