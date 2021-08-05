f3d89be0d91494a7a3a7c967965d877ad14e8807
MOBUMIN <kiju23@naver.com>
2021-08-05 15:02:48 +0900

    deploy test

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,13 @@ app.use(expressSession({
 }))
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.get('/', (req,res)=>{res.send('hi')})
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static("../frontend/build"));
+	app.get("*", (req,res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+	})
+}
+else app.get('/', (req,res)=>{res.send('hi')})
 app.use('/',router);
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
