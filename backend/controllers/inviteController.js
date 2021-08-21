f857ba5fbc109b4ea3e90496de9c27ab190c3e57
qf9ar8nv <ckguscjf0@gmail.com>
2021-08-21 15:45:19 +0900

    refactor: modify challenge in API
    * invite에서 사용하기 위해 challenge in API에서 사용하는 join 함수를 전역함수로 바꿈.
    * join함수에서 앞서 검사한 오류는 다시 검사하지 않도록 수정.
    * 코드의 가독성을 높이고 추후 구현한 함수와 비슷하게 재구성.

--- backend/controllers/inviteController.js
+++ backend/controllers/inviteController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModel');
 const Challenge = require('../models/challengeModel');
+const challengeController = require('./challengeController');
 const Alarm = require('../models/alarmModel');
 const Invite = require('../models/inviteModel');
 const authMailController = require('./authMailController');
@@ -20,48 +21,72 @@ let transporter = nodemailer.createTransport({
 });
 
 async function CreateInvite(req, res) {
-    try {
+	try {
 		const ch_id = req.params.challenge_id
 		const challenge_id = ObjectId(ch_id)
-        const { invite_email, send_user } = req.body;
+		const { invite_email, send_user } = req.body;
 
-        const user = await User.findOneByUsername(send_user);
-        if(user === null) throw 'not exist user'
+		const user = await User.findOneByUsername(send_user);
+		if (user === null) throw 'not exist user'
 
 		const challenge = await Challenge.findById(challenge_id);
-		if(challenge === null) throw 'not exist challenge'
+		if (challenge === null) throw 'not exist challenge'
 
 		const auth_num = authMailController.makeAuthNum(12)
 
 		const result = await transporter.sendMail({
-			from: `1day 1commit <${process.env.NODEMAILER_USER}>`, 
+			from: `1day 1commit <${process.env.NODEMAILER_USER}>`,
 			to: invite_email,
 			subject: `"${challenge.name}"에서 당신을 초대했습니다.`,
 			text: `링크 : http://localhost:5000/challenge/${challenge_id}/invite/${auth_num}`,
 		}, (err, info) => {
-			if(err) {
+			if (err) {
 				console.log(err)
 				return false
 			}
 		});
-		if(result === false) throw "메일 전송 실패"
+		if (result === false) throw "메일 전송 실패"
 
-		const invited_user = await User.findOne({user_email: invite_email})		// 가입된 유저면 알람에도 추가
-		if(invited_user) await Alarm.create(invited_user.user_id, `${challenge.name}에서 당신을 초대했습니다.`, `http://localhost:5000/challenge/${challenge_id}/invite/${auth_num}`);
+		const invited_user = await User.findOne({ user_email: invite_email })		// 가입된 유저면 알람에도 추가
+		if (invited_user) await Alarm.create(invited_user.user_id, `${challenge.name}에서 당신을 초대했습니다.`, `http://localhost:5000/challenge/${challenge_id}/invite/${auth_num}`);
 
 		await Invite.create(invite_email, ch_id, send_user, auth_num)
-		res.status(201).json({result: true})
-    } catch (err) {
+		res.status(201).json({ result: true })
+	} catch (err) {
 		console.log(err);
-        res.status(401).json({ error: err });
-    }
+		res.status(401).json({ error: err });
+	}
 }
 
 async function DeleteInvite(req, res) {		// mail 링크 누를 시 기능 구현
-	res.send("mail check")
+	try {
+		const ch_id = req.params.challenge_id;
+		const auth_num = req.params.auth_num;
+		const challenge_id = ObjectId(ch_id);
+
+		const invite = await Invite.findOne({ auth_num: auth_num })
+		if (invite === null) throw "잘못된 요청입니다."
+
+		const challenge = await Challenge.findById(challenge_id)
+		if (challenge === null || ch_id !== invite.challenge_id) throw "유효하지 않은 challenge입니다."
+
+		const user = await User.findOne({ user_email: invite.invite_email });
+		console.log(user)
+		if (user === null) throw "유효하지 않은 user입니다."
+
+		const join_result = await challengeController.join(challenge, user)
+		console.log(join_result)
+		if(join_result === false) throw "challenge와 user에 추가 실패"
+
+		res.status(201).json({ result: "성공" });
+	} catch (err) {
+		console.log(111)
+		console.log(err);
+		res.status(401).json({ error: err });
+	}
 }
 
 module.exports = {
-    createInvite: CreateInvite,
+	createInvite: CreateInvite,
 	deleteInvite: DeleteInvite,
 };
