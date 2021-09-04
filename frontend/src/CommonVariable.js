2384e27f275d98d6162b318b1fdaa6b5286f1a1e
sally0226 <sally734595@gmail.com>
2021-09-04 21:43:41 +0900

    chore : modify back api url

--- frontend/src/CommonVariable.js
+++ frontend/src/CommonVariable.js
@@ -5,4 +5,4 @@
  export const API_URL = "http://localhost:5000/api";
 
 // 배포용
-// export const API_URL = "https://alsolvechallenge.herokuapp.com"; // main branch에 develop merge할 때 /api 추가해야함 
+// export const API_URL = "https://alsolvechallenge.herokuapp.com/api"; // main branch에 develop merge할 때 /api 추가해야함 
